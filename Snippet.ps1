c9c4f691463b363edb9558e4422c7a20debc138b
Alexander <39201353+carloscondit@users.noreply.github.com>
2021-11-03 12:00:32 +0300

    Добавил пример параметра для проверки каталога

--- Snippet.ps1
+++ Snippet.ps1
@@ -4,3 +4,17 @@ $MsiExitCode = (Start-Process msiexec -ArgumentList "/i 'путь к msi' /quiet
 #В итоге получишь код выхода после установки, если 0, то всё оке, если 1603, то курим логи
 #Можно добавить ключ /norestart если есть шанс что установка захочет ребута. В этом случае если код выхода будет 3010, то нужен ребут после установки
 #Все коды выхода - https://docs.microsoft.com/ru-ru/windows/win32/msi/error-codes
+
+#Условия проверки каталога в параметр расширенной функции
+[Parameter(Position = 1, HelpMessage = "Укажите имя каталога")]
+[ValidateScript({
+        #это условие выдаст ошибку в случае, если проверка каталога не пройдет
+        If ((Test-Path -path $_ -PathType Container) -AND ((Get-Item -path $_).psprovider.name -eq 'Filesystem')) {
+            return $True
+        }
+        else {
+            Throw "The specified Path value $_ is not a valid folder or not a file system location."
+            return $False
+        }
+    })]
+[string]$Path = "."
